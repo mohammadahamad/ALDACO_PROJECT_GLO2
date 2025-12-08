1cd058b868c3d21c825e5e026872593b19fc0623
Damaris <damaris.barbot@utt.fr>
2025-12-08 10:27:43 +0100

    creation de vcard-spec.js

---
+++ spec/vcard-spec.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+describe("Program testing of search functionality", function () {
+  beforeAll(function () {});
+
+  it("should create a vcard", async function () {
+    const { createVcard } = await import("../src/vcard.js");
+
+  });
+});
